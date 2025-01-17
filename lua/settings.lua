47b1883a46901f0759b66521f514e59ed09e23c9
Jaideep6666 <msivarani7@gmail.com>
2025-01-17 11:42:05 +0530

    Add files via upload

---
+++ lua/settings.lua
@@ -0,0 +1,16 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.g.mapleader = " "
+-- Use PowerShell as the default shell
+vim.opt.shell = "powershell"
+vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+vim.opt.shellquote = "\""
+vim.opt.shellxquote = ""
+-- vim.opt.guifont = "JetBrainsMono Nerd Font:h12"
+-- vim.g.neovide_font = "JetBrainsMono Nerd Font:h12"
